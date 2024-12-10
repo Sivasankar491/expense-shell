54275eb582d5ce5f83c70f4ea73931046aedbcd7
Sankar491 <sivasankarrcks@gmail.com>
2024-12-10 06:15:59 +0530

    shell

---
+++ mysql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT() {
+    if [ $USER_ID -ne 0 ]
+    then    
+        echo -e "$YPlease run the script with root user$N"
+        exit 1
+}
+
+VALIDATE() {
+    if [ $? -ne 0 ]
+    then
+        echo "MySQL isn't installed"
+        exit 1
+fi
+}
+
+CHECK_ROOT
+
+
+
+
