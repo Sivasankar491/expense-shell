d4ec8cde21fee992d97ae8bece342ccf5f7857fb
Sankar491 <sivasankarrcks@gmail.com>
2024-12-10 06:50:55 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -18,17 +18,42 @@ CHECK_ROOT() {
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 installation is $R FAILED $N"
+        echo -e "$2 is $R FAILED $N"
         exit 1
     else
-        echo -e "$2 installation is $G SUCCESS $N"
+        echo -e "$2 is $G SUCCESS $N"
     fi
 }
 
 CHECK_ROOT
 
+dnf list installed|grep -i mysql
+VALIDATE $? "MySQL already installed"
+
 dnf install mysql-server -y
-VALIDATE $? MySQL
+VALIDATE $? "MySQL installation"
+
+systemctl enable mysqld
+VALIDATE $? "Enable MYSQL"
+
+systemctl start mysqld
+VALIDATE $? "Starting MYSQL"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 -e "show databases;"
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now"
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting up root password"
+else
+    echo -e "MySQL root password is already setup, $Y SKIPPING $N"
+fi
+    
+
+
+
+
 
 
 
